ac2857a7df6308e71405befabe6fc148be8a7ac9
shanesgh <103379145+shanesgh@users.noreply.github.com>
2025-07-09 20:34:13 -0400

    Updated __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,22 @@
-import { createRootRoute, Outlet } from '@tanstack/react-router';
-import { TanStackRouterDevtools } from '@tanstack/router-devtools';
+import { createFileRoute } from '@tanstack/react-router';
+import { Hero } from '../components/Hero';
+import { ClaimsProcessInfographic } from '../components/ClaimsProcessInfographic';
+import { Features } from '../components/Features';
+import { Pricing } from '../components/Pricing';
+import { CTA } from '../components/CTA';
 
-export const Route = createRootRoute({
-  component: () => (
-    <>
-      <Outlet />
-      <TanStackRouterDevtools />
-    </>
-  ),
+export const Route = createFileRoute('/')({
+  component: Index,
 });
+
+function Index() {
+  return (
+    <div className="space-y-0">
+      <Hero />
+      <ClaimsProcessInfographic />
+      <Features />
+      <Pricing />
+      <CTA />
+    </div>
+  );
+}
