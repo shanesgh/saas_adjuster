a361ed824d7169b086e6c9ce41a1beb6d0123258
shanesgh <103379145+shanesgh@users.noreply.github.com>
2025-07-15 22:52:28 -0400

    Updated FormNavigation.tsx

--- src/components/companies/icavs/navigation/FormNavigation.tsx
+++ src/components/companies/icavs/navigation/FormNavigation.tsx
@@ -16,7 +16,6 @@ export const FormNavigation = ({
   canSubmit = true,
   onGeneratePdf,
   customButtons,
-  customButtons,
 }: FormNavigationProps) => {
   const { goToPreviousStep, goToNextStep, isLastStep } = useForm();
 
